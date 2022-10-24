5c31ffe413bbcca8ce65348591900249f8901a87
搬砖达人小王 <rang1994@icloud.com>
2022-10-24 17:46:19 +0800

    dev: @types/react update to 18

--- src/demos/autoImport/index.tsx
+++ src/demos/autoImport/index.tsx
@@ -1,7 +1,12 @@
 function Counter() {
 	// unplugin-auto-import配置完成后出现红色语法警告(DONE: tsconfig.json include里添加"./auto-imports.d.ts")
 	const [count, setCount] = useState(0);
-	return <div>{count}</div>;
+	return (
+		<div>
+			测试文字
+			<div>{count}</div>
+		</div>
+	);
 }
 
 export default Counter;
