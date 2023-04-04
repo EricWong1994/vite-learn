88538e9218b1a84b66f56405a148a6be6845082c
搬砖达人小王 <rang1994@icloud.com>
2023-04-04 17:04:24 +0800

    feat: 国际化format

--- src/demos/i18ndemo/i18n.js
+++ src/demos/i18ndemo/i18n.js
@@ -1,8 +1,9 @@
-import i18n from 'i18next';
+import dayjs from 'dayjs';
+import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-i18n
+i18next
   // 检测用户当前使用的语言
   // 文档: https://github.com/i18next/i18next-browser-languageDetector
   .use(LanguageDetector)
@@ -12,6 +13,7 @@ i18n
   // 配置参数的文档: https://www.i18next.com/overview/configuration-options
   .init({
     debug: true,
+    // fallbackLng 指定了未匹配任何语言时的默认语言
     fallbackLng: 'en',
     interpolation: {
       escapeValue: false,
@@ -22,15 +24,37 @@ i18n
           // 这里是我们的翻译文本
           welcome: 'Welcome to my website',
           author: `Author is：<1>code and beast</1>`,
+          currentTime: 'Current time is {{time, DD/MM/YY}}',
+          "header": {
+            "title": "Title",
+            "subtitle": "Subtitle"
+          },
+          "intlNumber": "Some {{val, number}}",
+          "intlNumberWithOptions": "Some {{val, number(minimumFractionDigits: 2)}}",
+          letter: '转成小写字母: {{val, lowercase}}',
         }
       },
       zh: {
         translation: {
           welcome: '欢迎来到我的网站',
           author: `作者是：<1>代码与野兽</1>`,
+          currentTime: '当前时间是 {{time, YYYY-MM-DD}}',
         }
       }
     }
   });
 
-export default i18n;
+i18next.services.formatter.add('DD/MM/YY', (value, lng, options) => {
+  // console.log('dayjs: ', dayjs);// dayjs没引入居然不报错？
+  return dayjs(value).format('DD/MM/YY')
+});
+
+i18next.services.formatter.add('YYYY-MM-DD', (value, lng, options) => {
+  return dayjs(value).format('YYYY-MM-DD')
+});
+
+i18next.services.formatter.add('lowercase', (value, lng, options) => {
+  return value.toLowerCase();
+});
+
+export default i18next;
--- src/demos/i18ndemo/index.tsx
+++ src/demos/i18ndemo/index.tsx
@@ -1,4 +1,6 @@
+import dayjs from 'dayjs';
 import { useTranslation, Trans } from 'react-i18next';
+import i18next from 'i18next';
 
 const lngs = {
   en: { nativeName: 'English' },
@@ -18,6 +20,7 @@ function App() {
           )}
         </select>
       </header>
+      <h2>{t('header.title')}</h2>
       <main>
         <p>
           {t('welcome')}
@@ -26,6 +29,23 @@ function App() {
         <Trans i18nKey="author">
           作者是: <code>{Date.now()}</code>
         </Trans>
+        <h1>
+          {t('currentTime', { time: dayjs() })}
+        </h1>
+        <p>
+          {t('currentTime', { time: dayjs().format('MM/DD/YYYY') })}
+        </p>
+        <div>-----------------------</div>
+        <h2>官方示例：</h2>
+        <p>{i18next.t('intlNumber', { val: 1000 })}</p>
+        {/* // 保留三位小数 --> Some 1,000.100 */}
+        <p>{i18next.t('intlNumber', { val: 1000.1, minimumFractionDigits: 3 })}</p>
+        <p>{i18next.t('intlNumber', { val: 1000.1, formatParams: { val: { minimumFractionDigits: 3 } } })}</p>
+        <p>{i18next.t('intlNumberWithOptions', { val: 2000 })}</p>
+        <p>{i18next.t('intlNumberWithOptions', { val: 2000, minimumFractionDigits: 3 })}</p>
+
+        HelloWorld
+        <h2>{i18next.t('letter', { val: 'HelloWorld' })}</h2>
       </main>
     </div>
   );
